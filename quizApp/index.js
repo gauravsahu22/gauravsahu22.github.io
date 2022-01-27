723c3469567093c533da762604eefddce71586c7
gauravsahu22 <96310400+gauravsahu22@users.noreply.github.com>
2022-01-27 22:29:22 +0530

    MCQ Quiz WebSite

---
+++ quizApp/index.js
@@ -0,0 +1,24 @@
+function check(){
+    var c=0;
+    var q1=document.quiz.question1.value;
+    var q2=document.quiz.question2.value;
+    var q3=document.quiz.question3.value;
+    var q4=document.quiz.question4.value;
+    var q5=document.quiz.question5.value;
+    var result=document.getElementById("result");
+    var quiz=document.getElementById("quiz");
+    if(q1=="JavaScript"){c++; } 
+    if(q2=="Agra"){c++; } 
+    if(q3=="Delhi"){c++; } 
+    if(q4=="CoronaVirus"){c++; } 
+    if(q5=="07"){c++; } 
+    quiz.style.display="none";
+    
+    if(c>=4){
+        result.textContent="Yes";
+    } else if(c<=3 && c>=2){
+        result.textContent="Maybe";
+    } else{
+        result.textContent="No";
+    }
+  }
